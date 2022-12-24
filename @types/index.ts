febdf7854198514ec0979315ab850cdf5eb4e252
Mike Olie <aolie94@gmail.com>
2022-12-24 10:52:10 -0800

    interim commit; imported assets

--- @types/index.ts
+++ @types/index.ts
@@ -1,5 +1,19 @@
 import { StaticImageData } from "next/image";
 
+export interface SizeObject {
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+  "2xl": string;
+}
+
+export interface PATFont {
+  src: string;
+  weight: string;
+  style: string;
+}
+
 export interface IProduct {
   id: string;
   name: string;
---
+++ modules/useMediaQuery.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function useQuery(query: string): boolean {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  const handleChange = (e: any) => setMatches(e.matches);
+
+  useEffect(() => {
+    const m = window.matchMedia(query);
+
+    setMatches(m.matches);
+
+    m.addEventListener("change", handleChange);
+
+    return () => {
+      m.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  return !matches;
+}
+
+export default useQuery;
