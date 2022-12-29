a722bb0a349b7afe6a2a5d4465a7580fa9259d1e
Mike Olie <aolie94@gmail.com>
2022-12-28 18:18:12 -0800

    added initial styles for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
+
+import styles from "../styles/Header.module.css";
 
 export default function Header() {
   return (
-    <nav className="nav__container">
-      <article>
-        <Image
-          height={60}
-          width={120}
-          alt="custom logo"
-          src="/logo.png"
-        />
+    <nav className={styles.nav__container}>
+      <article className={styles.image__wrapper}>
+        <Image height={60} width={120} alt="custom logo" src="/logo.png" />
       </article>
+      <ul className={styles.nav__list}>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Book Us</li>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li className="buy-link">Buy</li>
+      </ul>
     </nav>
   );
 }
