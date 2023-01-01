3272b67160427d6ee1ef7745649b1ea942eab5c8
Mike Olie <aolie94@gmail.com>
2022-12-31 16:38:31 -0800

    created mobile navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,49 @@
+import { useRef, useState } from "react";
 import Image from "next/image";
-// import Link from "next/link";
+import { motion, useCycle } from "framer-motion";
 
-import { PTrainMenuLink } from "../@types";
+import useDimensions from "../modules/useDimensions";
 
 import styles from "../styles/Header.module.css";
+import { links } from "../constants";
 
-const links: Array<PTrainMenuLink> = [
-  {
-    label: "Home",
-    link: "#",
+const mobileNavStyles = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(0px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
   },
-  {
-    label: "Menu",
-    link: "#",
+};
+
+const linkVariants = {
+  open: {
+    transition: { staggerChildren: 0.8, delayChildren: 0.9 },
   },
-  {
-    label: "Book Us",
-    link: "#",
+  closed: {
+    transition: { staggerChildren: 0.5, staggerDirection: -2 },
   },
-  {
-    label: "About Us",
-    link: "#",
-  },
-  {
-    label: "Contact",
-    link: "#",
-  },
-  {
-    label: "Buy",
-    link: "#",
-  },
-];
+};
 
 const linksToUse = links.map((l) => <li key={l.label}>{l.label}</li>);
 
-export default function Header() {
+function Header() {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
+  const handleHeaderClick = () => toggleOpen();
+
   return (
     <nav className={styles.nav__container}>
       <article className={styles.image__wrapper}>
@@ -47,6 +56,23 @@ export default function Header() {
         />
       </article>
       <ul className={styles.nav__list}>{linksToUse}</ul>
+      <motion.article
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        custom={height}
+        ref={containerRef}
+        className={styles.mobile__nav}
+      >
+        <header onClick={handleHeaderClick}>Menu</header>
+        <motion.article
+          variants={mobileNavStyles}
+          className={styles.nav__overlay}
+        >
+          <motion.ul variants={linkVariants}>{linksToUse}</motion.ul>
+        </motion.article>
+      </motion.article>
     </nav>
   );
 }
+
+export default Header;
