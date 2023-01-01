da11b709e5620671f6f20f9a090e54312d74c74e
Mike Olie <aolie94@gmail.com>
2022-12-31 19:01:28 -0800

    updated footer with social images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
@@ -7,7 +9,7 @@ export default function Footer() {
         <header>Contact Us</header>
         <span>Return To Top</span>
       </section>
-      <article>
+      <article className={styles.footer__info_container}>
         <ul className={styles.footer__address}>
           <li>Find us at</li>
           <li>8780 19th Street #215</li>
@@ -15,7 +17,32 @@ export default function Footer() {
           <li>Phone: 909-994-7970</li>
           <li>Fax: 909-980-7279</li>
         </ul>
-        <ul></ul>
+        <ul className={styles.footer__social_icons}>
+          <li>
+            <Image
+              height={25}
+              width={20}
+              alt="Instagram Logo"
+              src="/instagram-icon.png"
+            />
+          </li>
+          <li>
+            <Image
+              height={25}
+              width={20}
+              alt="facebook Logo"
+              src="/facebook-icon.png"
+            />
+          </li>
+          <li>
+            <Image
+              height={25}
+              width={20}
+              alt="twitter Logo"
+              src="/twitter-icon.png"
+            />
+          </li>
+        </ul>
       </article>
     </footer>
   );
