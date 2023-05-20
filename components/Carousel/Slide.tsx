72c41acc5ff7155c68d23e68cae8cb24aa28b521
Mike Olie <aolie94@gmail.com>
2023-05-20 03:12:39 -0700

    Revert "updated carousel"
    This reverts commit 587f2cceaa6bbb3d14cef4cee9f5ad042d64a920.

--- components/Carousel/Slide.tsx
+++ components/Carousel/Slide.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 
 import styles from "../../styles/Embla.module.css";
 
+import imageByIndex from "../../modules/imageByIndex";
+
 interface SlideProps {
   index: number;
 }
 function Slide({ index }: SlideProps) {
+  const imageData = imageByIndex(index);
+  const { src } = imageData;
   return (
     <div className={styles.embla__slide}>
       <div className={styles.embla__slide__number}>
@@ -13,7 +17,7 @@ function Slide({ index }: SlideProps) {
       </div>
       <Image
         className={styles.embla__slide__img}
-        src={`/slide-${index + 1}.png`}
+        src={src}
         alt="slide image"
         width={300}
         height={600}
