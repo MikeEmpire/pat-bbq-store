73221da3e42c2645cd2159e4098859a010557b89
Mike Olie <aolie94@gmail.com>
2023-05-19 17:38:45 -0700

    formatted form and contact us section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,45 @@
+import Image from "next/image";
+
+import styles from "../styles/Footer.module.css";
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className={styles.footer}>
+      <header className={styles.footer_header}>Contact Us</header>
+      <section className={styles.footer_info_outer__container}>
+        <article className={styles.footer_info__container}>
+          <h6>Find us at</h6>
+          <h6>
+            8780 19th Street #215 <br />
+            Rancho Cucamonga, California 91701
+          </h6>
+          <h6>Phone: 909-994-7970</h6>
+          <h6>Fax: 909-980-7279</h6>
+        </article>
+        <article
+          className={styles.footer_info__container}
+          style={{ display: "flex", gap: "19px", marginBottom: "10px" }}
+        >
+          <Image
+            width={27}
+            height={34}
+            src="/twitter-icon.png"
+            alt="twitter link"
+          />
+          <Image
+            width={27}
+            height={34}
+            src="/instagram-icon.png"
+            alt="instagram link"
+          />
+          <Image
+            width={27}
+            height={34}
+            src="/facebook-icon.png"
+            alt="facebook link"
+          />
+        </article>
+      </section>
+    </footer>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { EmblaOptionsType } from "embla-carousel-react";
 
 import styles from "../styles/Home.module.css";
 
+import ContactForm from "../components/ContactForm";
 import Carousel from "../components/Carousel";
 
 import patImg from "../public/pat.png";
@@ -22,28 +23,82 @@ export default function Home() {
         />
         <Carousel slides={SLIDES} options={OPTIONS} />
         <section className={styles.about_us__container}>
-          <header>About Us</header>
-          <article>
-            <figure>
-              <Image
-                src={patImg}
-                alt="Pat Front Profile"
-                width={300}
-                height={300}
-              />
-              <figcaption>
-                Pat Paterson is the epitome of a man with who won’t let go of
-                his dream. It all began at age 10 when Pat a Minnesota native
-                with a hunger for BBQ asked his mom to let him experiment with
-                making his own sauce. His mom a single parent with 2 boys, Pat
-                and his brother Jason agreed and allowed Pat to experiment with
-                sauces.
-                <article className={styles.read__more__container}>
-                  <button>Read More</button>
-                </article>
-              </figcaption>
-            </figure>
-          </article>
+          <header className={styles.container__header}>About Us</header>
+          <h4 className={styles.about_us__header}>
+            For us, P Train&#39;s BBQ is a legacy that is an honor to represent.
+            What started as Pat “Ptrain” Patterson&#39;s dream of serving real
+            wood smoked pizzas, his original bbq sauces and the perfect Tri Tip
+            sandwich has turned into so much more.{" "}
+          </h4>
+          <figure className={styles.history__container}>
+            <header className={styles.history__header}>Our History</header>
+            <Image
+              src={patImg}
+              alt="Pat Front Profile"
+              width={300}
+              height={300}
+            />
+            <figcaption>
+              When our dad was diagnosed and shortly later passed away from
+              cancer suddenly in 2021, it was a no-brainer to continue that
+              dream. I&#39;ve been smoking BBQ since I was 11, early
+              mornings-late nights with my dad. He taught us that with hard
+              work, patience and love you can achieve any dream that you have.
+              We worked at festivals, lunches, weddings, football games,
+              conventions… learning the invaluable skills he left us with.{" "}
+            </figcaption>
+          </figure>
+        </section>
+        <section className={styles.section__container}>
+          <header className={styles.container__header}>Menu</header>
+          <h6>
+            If you&#39;re looking for mouth-watering barbecue that&#39;s sure to
+            satisfy your cravings, look no further than P Trains BBQ. Our
+            catering services are perfect for any occasion, whether you&#39;re
+            hosting a backyard cookout, corporate event, or wedding reception.
+          </h6>
+          <br />
+          <figure className={styles.menu__figure}>
+            <Image
+              height={400}
+              width={200}
+              alt="P Train's BBQ Menu"
+              src="/CateringMenu.jpg"
+            />
+            <figcaption>
+              At P Trains BBQ, we take pride in using only the highest quality
+              ingredients and traditional smoking techniques to create our
+              delicious meats. From succulent pulled pork and tender brisket to
+              juicy ribs and savory chicken, our menu has something for
+              everyone.
+              <br />
+              <br />
+              We also offer a variety of sides to complement your meal, such as
+              creamy mac and cheese, tangy coleslaw, and savory baked beans. And
+              don&#39;t forget about our signature sauces, which are sure to
+              take your taste buds to the next level.
+            </figcaption>
+          </figure>
+        </section>
+        <section
+          className={styles.section__container}
+          style={{ padding: "0 6%", textAlign: "center" }}
+        >
+          <header className={styles.container__header}>Book Us</header>
+          <h6>
+            If you&#39;re ready to book P Trains BBQ for your upcoming event, or
+            if you have any questions about our catering services, we&#39;d love
+            to hear from you! Simply fill out the contact form below and one of
+            our friendly team members will be in touch shortly. <br /> <br />
+            We understand that planning an event can be stressful, which is why
+            we&#39;re committed to making the catering process as easy and
+            seamless as possible. By filling out the contact form, you&#39;ll be
+            taking the first step toward a delicious and stress-free event.
+          </h6>
+        </section>
+        <section className={styles.section__container}>
+          <header className={styles.container__header}>Get in touch</header>
+          <ContactForm />
         </section>
       </section>
       <Script src="https://cdn.snipcart.com/themes.v3.2.0/default/snipcart.js" />
