32c77e6f2c26e9f06a93fcff40c97c27d887918a
Michael Olie <aolie94@gmail.com>
2024-06-03 14:24:02 -0700

    rebased content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,11 @@ export default function Layout({ children }: PropsWithChildren<any>) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <script src="https://cdn.tailwindcss.com"></script>
+<<<<<<< HEAD
+=======
+        <link rel="preconnect" href="<https://app.snipcart.com>" />
+        <link rel="preconnect" href="<https://cdn.snipcart.com>" />
+>>>>>>> 6fd6804 (rebased content)
         <link rel="shortcut icon" href="../public/favicon.ico" />
       </Head>
       <Header />
