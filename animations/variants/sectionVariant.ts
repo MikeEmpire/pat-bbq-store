b029b0f30bf594cbb4e04031d3a4a21132e50f6c
Michael Olie <aolie94@gmail.com>
2025-01-25 09:41:54 -0800

    Modularizing components for new scalable structure

---
+++ animations/variants/sectionVariant.ts
@@ -0,0 +1,4 @@
+export default {
+  visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+  hidden: { opacity: 0, y: -50 },
+};
---
+++ pages/about/index.tsx
@@ -0,0 +1,5 @@
+import About from "../../components/About/About";
+
+export default function AboutUsPage(): JSX.Element {
+  return <About />;
+}
