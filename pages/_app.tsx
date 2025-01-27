817a974cb8bfcede0c75977a0894ed57f9bdd6ac
Michael Olie <aolie94@gmail.com>
2025-01-27 06:31:18 -0800

    Removed unneccessary components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,13 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Script src="https://cdn.tailwindcss.com" />
       <Script
         type="text/javascript"
-<<<<<<< HEAD
-=======
-        src="https://cdn.snipcart.com/themes.v3.2.0/default/snipcart.js"
-      />
-      <Script
-        type="text/javascript"
->>>>>>> f1be080 (updated images and added tailwind for responsive design. Also updated nav links)
         src="https://assets.calendly.com/assets/external/widget.js"
         async
       />
