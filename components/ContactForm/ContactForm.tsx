1685d7596dbe7fe049259e0d7f56ddaf125cf3ef
Michael Olie <aolie94@gmail.com>
2025-02-03 09:09:15 -0800

    updated contact form

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -77,78 +77,80 @@ const ContactForm: React.FC = () => {
           taking the first step toward a delicious and stress-free event.
         </h6>
       </div>
-      <form className="w-full max-w-lg p-8 rounded-lg shadow-lg">
-        <div className="mb-6">
-          <input
-            placeholder="Your Name Goes Here"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
-          />
-        </div>
-
-        <div className="mb-6">
-          <input
-            placeholder="Email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            type="email"
-            id="email"
-            name="email"
-            className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            placeholder="310-129-2938"
-            value={phoneNumber}
-            onChange={(e) => {
-              setPhoneNumber(e.target.value);
-            }}
-            type="text"
-            id="phoneNumber"
-            name="phoneNumber"
-            className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
-          />
-        </div>
-
-        <div className="mb-6">
-          <textarea
-            placeholder="Enter a message"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-            id="message"
-            name="message"
-            rows={4}
-            className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
-          />
-        </div>
-        {responseMessage && (
-          <div className={`mb-6 text-center ${responseTextClass}`}>
-            {responseMessage}
+      <div className="flex items-center justify-center min-h-min">
+        <form className="w-full max-w-lg p-8 rounded-lg shadow-lg">
+          <div className="mb-6">
+            <input
+              placeholder="Your Name Goes Here"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
+            />
           </div>
-        )}
 
-        <div className="flex justify-end">
-          <button
-            disabled={!isReady}
-            onClick={handleConfirm}
-            type="button"
-            className="bg-indigo-500 text-white font-medium py-2 px-4 rounded hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Send
-          </button>
-        </div>
-      </form>
+          <div className="mb-6">
+            <input
+              placeholder="Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="email"
+              id="email"
+              name="email"
+              className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              placeholder="310-129-2938"
+              value={phoneNumber}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+              type="text"
+              id="phoneNumber"
+              name="phoneNumber"
+              className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
+            />
+          </div>
+
+          <div className="mb-6">
+            <textarea
+              placeholder="Enter a message"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              id="message"
+              name="message"
+              rows={4}
+              className="w-full border-b-2 border-white-300 focus:border-indigo-500 focus:outline-none py-2 px-4"
+            />
+          </div>
+          {responseMessage && (
+            <div className={`mb-6 text-center ${responseTextClass}`}>
+              {responseMessage}
+            </div>
+          )}
+
+          <div className="flex justify-end">
+            <button
+              disabled={!isReady}
+              onClick={handleConfirm}
+              type="button"
+              className="bg-indigo-500 text-white font-medium py-2 px-4 rounded hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
