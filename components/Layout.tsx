3e77b196c16c192211fa964de9588d8448c9be82
Michael Olie <aolie94@gmail.com>
2025-10-10 21:19:53 -0700

    performance: Added necessary google tags for analytics purposes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren } from "react";
 
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Script from "next/script";
 
 export default function Layout({ children }: PropsWithChildren<any>) {
   return (
@@ -18,7 +19,28 @@ export default function Layout({ children }: PropsWithChildren<any>) {
         <link rel="preconnect" href="<https://cdn.snipcart.com>" />
         <link rel="shortcut icon" href="../public/favicon.ico" />
       </Head>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5GXHQH2R');
+          `,
+        }}
+      />
       <Header />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5GXHQH2R"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       <main>{children}</main>
       <Footer />
     </>
